104653cf14888b00230f0d8fcbe3e0a86e2b3a22
李京彧 <liujinlisb@gmail.com>
2024-10-16 00:58:17 +0800

    Update ArtalkComponent.js

--- ArtalkComponent.js
+++ ArtalkComponent.js
@@ -4,17 +4,34 @@ import React, { useEffect } from 'react';
 const ArtalkComponent = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const Artalk = require('artalk').default;
-      Artalk.init({
-        el: '#Comments',
-        pageKey: '/post/1',
-        pageTitle: '关于引入 Artalk 这档子事',
-        // 其他配置项
-      });
+      const script = document.createElement('script');
+      script.src = 'http://localhost:8080/dist/Artalk.js';
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        const Artalk = window.Artalk;
+        Artalk.init({
+          el: '#Comments',
+          pageKey: '/post/1',
+          pageTitle: '关于引入 Artalk 这档子事',
+          server: 'http://localhost:8080',
+          site: 'Artalk 的博客',
+        });
+      };
+
+      return () => {
+        document.body.removeChild(script);
+      };
     }
   }, []);
 
-  return <div id="Comments"></div>;
+  return (
+    <>
+      <link href="http://localhost:8080/dist/Artalk.css" rel="stylesheet" />
+      <div id="Comments"></div>
+    </>
+  );
 };
 
 export default ArtalkComponent;
